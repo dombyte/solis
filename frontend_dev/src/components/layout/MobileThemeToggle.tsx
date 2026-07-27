f41e624ace12b0d03cd2a8f8ae84cf155e4168f4
dombyte <dom@ixsrv.de>
2026-07-27 14:50:13 +0200

    remove old ui

--- frontend_dev/src/components/layout/MobileThemeToggle.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemeToggle } from './ThemeToggle';
-
-export function MobileThemeToggle() {
-  return (
-    <div className="flex items-center w-fit">
-      <ThemeToggle />
-    </div>
-  );
-}
